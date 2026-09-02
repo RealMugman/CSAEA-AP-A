eaf2b16e6cbbe61a4a1ab4b1d9fbe07ed058bf09
RealMugman <no8780031@gmail.com>
2026-09-02 12:04:36 +0000

    a

---
+++ CrashCourse/CrashCourse.java
@@ -0,0 +1,16 @@
+package CrashCourse;
+
+public class CrashCourse {   //Class header
+    
+    public static void main(String[] args){
+
+        // comments 
+
+         /*Multiline
+        comment
+        here */
+
+        System.out.print("Hello");
+    }
+
+}
