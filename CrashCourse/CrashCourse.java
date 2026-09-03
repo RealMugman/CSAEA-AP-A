d6ba4f4483cd0589fcc91b693782edb3788dc75c
RealMugman <no8780031@gmail.com>
2026-09-03 13:38:23 +0000

    9/3 arrays

--- CrashCourse/CrashCourse.java
+++ CrashCourse/CrashCourse.java
@@ -41,7 +41,66 @@ public class CrashCourse {   //Class header
 
         //Comparisons (always return true or false)
         // < > <= >= == != 
-        System.out.println(4 < 5); 
-    }
+        System.out.println(4 < 5);
+        System.out.println(7 == 4);
+        System.out.println(1 != 2); 
+
+        // logical opperators
+        // !     &&    ||
+        // Not  ANd    OR
+
+        boolean e = false; boolean f = true;
+
+        // predict output: true or false?
+
+        System.out.println(!e); // True
+        System.out.println(!e && f); // False
+        System.out.println(!e || f); // True
+        System.out.println(e || f && !e); // True 
+
+        System.out.println(e && f); //Short curcuits after checking e -- Java does not need to
+
+        // Casting (converting)
+        int g = (int)5.5;
+        System.out.println(g);
+        double h = (double)5/6;
+        System.out.println(h);
+        
+        //Strings
+        
+        String s1 = "Goodnight";
+        String s2 = " and "; 
+        String s3 = "Goodbye"; 
+        String result = s1 + s2 + s3;
+        result += ", Cowboy ."; 
+        System.out.println(result + '\n');
+
+        //
+        int[] arry1 = new int[10];
+        System.out.println(arry1);
+        System.out.println("length: " + arry1.length);
+
+        arry1[0] = 11;
+        arry1[1] = 2;
+
+        System.out.println("Remainder: " + arry1[0] % arry1[1]);
+
+        int[] arry2 = {32, 52, 3, 64, 32};
+        System.out.println(arry2);
+        
+        //2D arrays (grid or a table)
+
+        int[][] arrayGrid1 = new int[4][3];
+        // 0 0 0
+        // 0 0 0
+        // 0 0 0
+        // 0 0 0 
+
+        System.out.println("Rows:" + arrayGrid1.length);
+        System.out.println("Collumns:" + arrayGrid1[0].length);
+
+
+
+        }
 
 }
