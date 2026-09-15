9d06895a40c506863f376597671afc20e40bd490
RealMugman <no8780031@gmail.com>
2026-09-15 12:36:50 +0000

    Fruit

---
+++ CrashCourse/Fruit.java
@@ -0,0 +1,14 @@
+package CrashCourse;
+
+public class Fruit {
+    String name;
+    boolean isSweet;
+    boolean isSour;
+    String color;
+    int age;
+    boolean isPickedFromGround;
+    boolean isPickedFromTree;
+    boolean isRipe;
+
+
+}
