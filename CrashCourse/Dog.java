542f284475f0310f8e74be79af2d345720e742f0
RealMugman <no8780031@gmail.com>
2026-09-15 12:26:59 +0000

    9/15

---
+++ CrashCourse/Dog.java
@@ -0,0 +1,39 @@
+package CrashCourse;
+
+public class Dog {
+    
+    String characterName;
+    String ownerName;
+    String breed;
+    int age;
+    double weight;
+    boolean isHungry;
+    boolean isHome;
+    int energyLevel;
+
+    public Dog(String characterName, String breed, int age) {
+        this.characterName = characterName;
+        this.breed = breed;
+        this.age = age; 
+    }
+
+    public void eat() {
+        energyLevel += 15;
+        if (energyLevel > 100) {
+            energyLevel = 100;
+        }
+    }
+    public void sleep() {
+        energyLevel += 25;
+        if (energyLevel > 100) {
+            energyLevel = 100;
+        }
+    }
+    public void bark() {
+        energyLevel -= 1;
+        if (energyLevel < 0) {
+            energyLevel += 1;
+        }
+    }
+    }
+
