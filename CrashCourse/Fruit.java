31a137485cbb95f7f6d4be8a209daca82cf0b0f4
RealMugman <no8780031@gmail.com>
2026-09-15 12:52:01 +0000

    Fruit1

--- CrashCourse/Fruit.java
+++ CrashCourse/Fruit.java
@@ -9,6 +9,30 @@ public class Fruit {
     boolean isPickedFromGround;
     boolean isPickedFromTree;
     boolean isRipe;
+    boolean isEat;
+    boolean isHarvest;
 
+    public void fruit(String name, boolean isRipe, int age) {
+        this.name = name;
+        this.isRipe = isRipe;
+        this.age = age;
+    }
 
+    public void eat() {
+        isEat = true;
+    }
+    public void Pick() {
+        isRipe = true;
+    }
+    public void Bad() {
+        isRipe = false;
+    }
+    public void Grown() {
+        isHarvest = true;
+    }
+    public void Growing() {
+        isHarvest = false; 
+    }
 }
+
+
